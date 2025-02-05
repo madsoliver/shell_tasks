c0570d7b573a4689a4de328c02a3510de21eff52
Mads Oliver Jenssen <30602638+madsoliver@users.noreply.github.com>
2025-02-05 13:44:47 +0100

    Update cpanel_cleanup.sh

--- cpanel_cleanup.sh
+++ cpanel_cleanup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Sjekk størrelse før opprydding
+BEFORE=$(du -sh /home | awk '{print $1}')
+echo "Størrelse før opprydding: $BEFORE"
+
 # Fjern loggfiler i sc* katalogene
-rm -f /home/sc*/logs/*
+rm -f sc*/logs/*
 
 # Fjern error_log filer i www og kontrollpanel
 rm -f /home/sc*/www/error_log
@@ -11,6 +15,7 @@ rm -f /home/sc*/www/kontrollpanel/error_log
 rm -f /home/sc*/*.tar.gz
 
 # Fjern var/logs kataloger og innhold
+rm -rf /home/sc*/www/var/log
 rm -rf /home/sc*/www/var/logs
 
 # Fjern cache dev kataloger
@@ -21,5 +26,12 @@ rm -f /home/sc*/www/modules/*.zip
 
 # Fjern alt i trash-mapper
 rm -rf /home/sc*/.trash/*
+
+# Sjekk størrelse etter opprydding
+AFTER=$(du -sh /home | awk '{print $1}')
+echo "Størrelse etter opprydding: $AFTER"
+
+# Beregn frigjort plass
+echo "Frigjort plass: $(numfmt --to=iec $(( $(du -sb /home | awk '{print $1}') - $(du -sb /home | awk '{print $1}') )))"
 
 echo "Opprydding fullført!"
